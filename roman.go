f341b466b7c3438766b87fe02b906cf36f326dde
anand23vs <132248672+anand-gec@users.noreply.github.com>
2026-09-03 01:01:03 +0530

    Roman nuber

---
+++ roman.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func romanToInt(s string) int {
+	m := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	result := 0
+	for i := 0; i < len(s); i++ {
+		if i+1 < len(s) && m[s[i]] < m[s[i+1]] {
+			result -= m[s[i]]
+		} else {
+			result += m[s[i]]
+		}
+	}
+	return result
+}
+func main() {
+	fmt.Println("Input like: I V X L C D M ")
+	var num string
+	fmt.Scan(&num)
+	fmt.Printf("Roman number of %s is: %d", num, romanToInt(num))
+}
