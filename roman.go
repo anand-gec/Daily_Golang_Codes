fac44404d3a602e860f13a1b592be662f77c3eaf
anand23vs <132248672+anand-gec@users.noreply.github.com>
2026-09-06 00:39:49 +0530

    new

--- roman.go
+++ roman.go
@@ -6,13 +6,7 @@ import (
 
 func romanToInt(s string) int {
 	m := map[byte]int{
-		'I': 1,
-		'V': 5,
-		'X': 10,
-		'L': 50,
-		'C': 100,
-		'D': 500,
-		'M': 1000,
+		'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
 	}
 	result := 0
 	for i := 0; i < len(s); i++ {
