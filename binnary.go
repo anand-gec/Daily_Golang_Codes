97b4ede5549c62070922d25bc3ecabbe557f36bf
anand23vs <132248672+anand-gec@users.noreply.github.com>
2026-09-07 01:08:04 +0530

    Binaery search

--- binnary.go
+++ binnary.go
@@ -4,25 +4,27 @@ import (
 	"fmt"
 )
 
-func binarySearch(arr []int, target int) int {
+func binarySearch(arr []int, target int) []int {
+	var arr2 = []int{}
 	start := 0
 	end := len(arr) - 1
 	for start <= end {
 		mid := (start + end) / 2
 		if arr[mid] == target {
-			return arr[mid]
+			arr2 = append(arr2, mid)
+			break
 		} else if arr[mid] < target {
 			start = mid + 1
 		} else {
 			end = mid - 1
 		}
 	}
-	return -1
+	return arr2
 }
 
 func main() {
 	arr := [5]int{2, 3, 6, 7, 9}
-	target := 6
+	target := 7
 
-	fmt.Println(binarySearch(arr[:], target))
+	fmt.Println("index of arr is : arr", binarySearch(arr[:], target))
 }
