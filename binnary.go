c6960c07235a6d656424a69fb2baa7fb4089b8b0
anand23vs <132248672+anand-gec@users.noreply.github.com>
2026-09-08 02:22:10 +0530

    reverse string

--- binnary.go
+++ binnary.go
@@ -4,27 +4,39 @@ import (
 	"fmt"
 )
 
-func binarySearch(arr []int, target int) []int {
-	var arr2 = []int{}
+func binarySearch(arr []int, target int) bool {
 	start := 0
 	end := len(arr) - 1
 	for start <= end {
 		mid := (start + end) / 2
 		if arr[mid] == target {
-			arr2 = append(arr2, mid)
-			break
+			return true
 		} else if arr[mid] < target {
 			start = mid + 1
 		} else {
 			end = mid - 1
 		}
 	}
-	return arr2
+	return false
 }
 
-func main() {
-	arr := [5]int{2, 3, 6, 7, 9}
-	target := 7
+// func mergeShort(arr []int) []int {
+// 	var arr3 = []int{}
+// 	start := 0
+// 	end := len(arr) - 1
+// 	mid := (start + end) / 2
+// 	for
 
+// 	return arr3
+// }
+
+func main() {
+	arr := [...]int{2, 3, 6, 7, 9}
+	target := 7
+	fmt.Println("BinarySearch")
 	fmt.Println("index of arr is : arr", binarySearch(arr[:], target))
+
+	// arr2 := [...]int{6, 8, 2, 5, 3, 9, 1}
+	// fmt.Println("MergeShort")
+	// fmt.Println(mergeShort(arr2[:]))
 }
